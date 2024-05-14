9baa49ee2a601e1b3ae0728ac407fe5ad81026c6
dlskrud815 <dlskrud815@gmail.com>
2024-05-14 20:20:36 +0900

    Add project files.

---
+++ 240514_cpp_practice1/240514_cpp_practice1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include "Monster.h"
+
+using namespace std;
+
+int main()
+{
+	Monster tempMonster;
+
+	Fighter tempFighter;
+	Archer tempArcher;
+	Wizard tempWizard;
+
+	tempMonster.m_HP; //부모 클래스 public 필드 상속x, 전체 사용 가능
+	tempMonster.m_MP; //부모 클래스 private 필드 상속x, 본 클래스에서만 사용
+	tempMonster.m_DEF; //부모 클래스 protected 필드 상속x, 본 클래스 및 상속 시에만 사용
+
+	//public 상속
+	tempFighter.m_HP; //부모 클래스 public 필드 상속 자식 클래스 public
+	tempFighter.m_MP; //부모 클래스 private 필드 상속 자식 클래스 private
+	tempFighter.m_DEF; //부모 클래스 protected 필드 상속 자식 클래스 protected
+	
+	//priavte 상속
+	tempArcher.m_HP; //부모 클래스 public 필드 상속 자식 클래스 private
+	tempArcher.m_MP; //부모 클래스 private 필드 상속 자식 클래스 private
+	tempArcher.m_DEF; //부모 클래스 protected 필드 상속 자식 클래스 private
+
+	//protected 상속
+	tempWizard.m_HP; //부모 클래스 public 필드 상속 자식 클래스 protected
+	tempWizard.m_MP; //부모 클래스 private 필드 상속 자식 클래스 private
+	tempWizard.m_DEF; //부모 클래스 protected 필드 상속 자식 클래스 protected
+}
---
+++ 240514_cpp_practice1/Monster.h
@@ -0,0 +1,67 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Monster
+{
+public:
+	string m_Name;
+	int m_HP;
+private:
+	int m_MP;
+protected:
+	int m_DEF;
+
+public:
+	int GetHP() { return m_HP; }
+	int GetMP() { return m_MP; }
+	virtual void Func()
+	{
+
+	}
+};
+
+class Fighter : public Monster
+{
+public:
+	void Func() override
+	{
+		m_HP; //public
+		m_MP; //private 클래스 밖 사용 x
+		m_DEF; //protected 상속 ok
+	}
+};
+
+class Archer : private Monster
+{
+private:
+	void Func() override
+	{
+		m_HP; //public
+		m_MP; //private 클래스 밖 사용 x
+		m_DEF; //protected 상속 ok
+	}
+};
+
+class Wizard : protected Monster
+{
+protected:
+	void Func() override
+	{
+		m_HP; //public
+		m_MP; //private 클래스 밖 사용 x
+		m_DEF; //protected 상속 ok
+	}
+};
+
+class Witch : public Wizard
+{
+protected:
+	void Func() override
+	{
+		m_HP; //public
+		m_MP; //private 클래스 밖 사용 x
+		m_DEF; //protected 상속 ok
+	}
+};
